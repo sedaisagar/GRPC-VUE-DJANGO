3a2d46efba2baf33cab5f8566ee6b77c350a23d0
leokingsagarbro <leokingsagarbro@gmail.com>
2025-01-18 18:48:39 +0545

    update

--- todos/settings.py
+++ todos/settings.py
@@ -31,13 +31,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
-# CORS_ALLOW_ALL_ORIGINS = True
-
-CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:3000",
-    "http://localhost:3000",
-    "http://localhost:50051",
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = "todos.urls"
 
